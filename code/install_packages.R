0038df748272348e33d286433a40c2761a51a3af
Elliot Outland <ehoutland@gmail.com>
2021-06-24 14:36:04 -0500

    updating lockfile, adding limorhyde2/ashr to renv

--- code/install_packages.R
+++ code/install_packages.R
@@ -1,4 +1,13 @@
 # renv::init()
+
+# if (!requireNamespace('remotes', quietly = TRUE))
+#   install.packages('remotes')
+# remotes::install_github('stephens999/ashr')
+# 
+# if (!requireNamespace('BiocManager', quietly = TRUE))
+#   install.packages('BiocManager')
+# BiocManager::install('limorhyde2')
+
 # renv::snapshot()
 
 renv::restore()
